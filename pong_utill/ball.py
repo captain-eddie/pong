e266df0b51aa4caae71a5dcf9042fb489fbfbc25
captain-eddie <edward.guobadia@gmail.com>
2022-11-28 18:56:21 -0500

    Working on ball collision
    It collides with the top and bottom of the screen but it does not succsefully bounce off the edge. Working on that

--- pong_utill/ball.py
+++ pong_utill/ball.py
@@ -10,27 +10,46 @@ class Ball():
     def __init__(self, surface, pos = [500, 300]):
         self.surface = surface
         self.pos = pos
+        self.hitbox_pos = [485, 285]
+        self.radius = 15
+        self.hitbox = pygame.Rect(self.hitbox_pos[0], self.hitbox_pos[1], 2 * self.radius, 2 * self.radius)
+        
+        self.bounds = self.surface.get_rect()
+        
+        #self.border_rect = pygame.display.get_surface().get_rect()
+        #self.hitbox.clamp_ip(self.border_rect)
 
     def draw_to_screen(self):
-        pygame.draw.circle(self.surface, (255, 255, 255), self.pos, 15)
+        pygame.draw.circle(self.surface, (255, 255, 255), self.pos, self.radius)
+        pygame.draw.rect(self.surface, (255, 0, 0), self.hitbox, 2)
     
     def move(self, speed, angle = 360):
-        if angle == 360:
             angle = angle * speed        
             xSpeed = speed * sin(angle)
             ySpeed = speed * cos(angle)
+
+            if self.pos[1] - self.radius < self.bounds.top or self.pos[1] + self.radius > self.bounds.bottom:
+                ySpeed *= -1 
+                xSpeed *= -1
+
             self.pos[0] += xSpeed
-            self.pos[1] += ySpeed
-            if self.pos[1] == 15 or self.pos[1] == 585:
-                self.move(speed, angle = 90)
-        elif angle == 90:
-            angle = angle * speed        
-            xSpeed = speed * sin(angle)
-            ySpeed = speed * cos(angle)
-            self.pos[0] += xSpeed
-            self.pos[1] += ySpeed
-            if self.pos[1] == 15 or self.pos[1] == 585:
-                self.move(speed, angle = 45)
+            self.pos[1] -= ySpeed
+            self.hitbox.center = self.pos
+
+            #if self.hitbox[0] < self.bounds.left or self.hitbox[0] > self.bounds.left:
+            #    #self.hitbox.vel_x *= -1 
+            #    xSpeed *= - 1
+            #if self.hitbox[1] < self.bounds.top or self.hitbox[1] > self.bounds.top:
+            #    #self.hitbox.vel_y *= -1
+            #    ySpeed *= -1
+
+            #if self.pos[0] - self.radius < self.bounds.left or self.pos[0] + self.radius > self.bounds.right:
+            #    xSpeed *= -1 
+
+            #if self.pos[1] + self.radius >= 600:
+            #    ySpeed *= -1
+            #elif self.pos[1] + self.radius <= 0:
+            #    ySpeed *= -1
 
 
     def reset(self, speed):
