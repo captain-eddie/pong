0d5e59bb8b60351fe62b94f9fedc22a522e45a14
captain-eddie <edward.guobadia@gmail.com>
2022-11-28 12:47:07 -0500

    Done with paddles
    On to the ball. This might be the fastest project I have ever made. I somehow get work done when I just shutup and code

--- main.py
+++ main.py
@@ -12,39 +12,49 @@ from pong_utill import paddle
 from sys import exit
 from time import sleep
 
+def player_movement(keys, player1, player2):
+    #   player1
+    if keys[pygame.K_w]:
+        player1.move(up = True)
+    if keys[pygame.K_s]:
+        player1.move(up = False)
+    #   player 2
+    if keys[pygame.K_UP]:
+        player2.move(up = True)
+    if keys[pygame.K_DOWN]:
+        player2.move(up = False)
+
 def main():
     #   initialize game window
     pygame.init()
+
     screen = pygame.display.set_mode((1000, 600))
     pygame.draw.line(screen, (255, 255, 255), (500, 0), (500, 600), 5)
 
+    #   player1 paddle(left side) player2 paddle(right side)
+    player1 = paddle.Paddle(screen, (10, 250))
+    player2 = paddle.Paddle(screen, (974, 250))
+
     #   main game loop
     while True:
-        #   player1 paddle(left side) player2 paddle(right side)
-        player1 = paddle.Paddle(screen, (10, 250))
-        player2 = paddle.Paddle(screen, (974, 250))
+        #   updates the game window
+        pygame.display.update()
+        screen.fill((0, 0, 0))
+        pygame.draw.line(screen, (255, 255, 255), (500, 0), (500, 600), 5)
+
+        #   draws paddles to screen
+        player1.draw_to_screen(screen)
+        player2.draw_to_screen(screen)
 
         #   event handler
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
 
-            #   paddle movement w,s for p1 up,down for p2
-            key = event.type
-            if key == pygame.KEYDOWN:
-                while event.type == pygame.K_w:
-                    player1 = player1.move(event.key)
-                if event.type == pygame.K_s:
-                    player1 = player1.move(event.key)
-                if event.type == pygame.K_UP:
-                    player2 = player2.move(event.key)
-                if event.type == pygame.K_DOWN:
-                    player2 = player2.move(event.key)
-                sleep(0.1)
+        #   paddle movement w,s for p1 up,down for p2
+        keys_pressed = pygame.key.get_pressed()
+        player_movement(keys_pressed, player1, player2)
 
-
-        #   updates the game window
-        pygame.display.update()
 
 if __name__ == "__main__":
     main()
--- pong_utill/paddle.py
+++ pong_utill/paddle.py
@@ -9,16 +9,18 @@ class Paddle():
     def __init__(self, surface, pos):
         self.x, self.y = pos
         self.surface = surface
-        self.paddle = pygame.Rect(self.x, self.y, 16, 100)
-        pygame.draw.rect(surface, (255, 255, 255), self.paddle)
 
     #   paddle movement
-    def move(self, direction):
-        if direction == pygame.K_w or direction == pygame.K_UP:
-            self.y -= 4
-            print("UP")
-        elif direction == pygame.K_s or direction == pygame.K_DOWN:
-            self.y += 4
-            print("DOWN")
+    def move(self, up = True):
+        if up:
+            self.y -= 1
+            print(self.y)
+        elif not up:
+            self.y += 1
+            print(self.y)
 
-        return Paddle(self.surface, (self.x, self.y))
+        #return Paddle(self.surface, (self.x, self.y))
+
+    def draw_to_screen(self, surface):
+        self.paddle = pygame.Rect(self.x, self.y, 16, 100)
+        pygame.draw.rect(surface, (255, 255, 255), self.paddle)
